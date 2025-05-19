01abbd75510097b2001304f5c5c569fa43891a14
CrovArkhangel <SaToShi.KR.1123@gmail.com>
2025-05-19 12:28:55 +0900

    create car class

---
+++ src/main/java/com/example/Car.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Car {
+    private Integer Speed;
+
+    public Integer getSpeed() {
+        return Speed;
+    }
+
+    public void setSpeed(Integer speed) {
+        Speed = speed;
+    }
+}
